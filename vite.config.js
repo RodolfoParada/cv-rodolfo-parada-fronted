6d3c0959cd9c6407d48c5cd073ce2b8179615d94
Rodolfo Parada <rodolfo.parada.gonzalez@gmail.com>
2024-11-07 21:39:45 -0300

    v

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: 'https://github.com/RodolfoParada/cv_Rodolfo_Parada-frontend',
+ base: '/cv_Rodolfo_Parada-frontend/',
   plugins: [vue()],
   server: {
     port: 5174,  // Cambia el puerto a 5174
